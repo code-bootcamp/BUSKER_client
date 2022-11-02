3af8768fd466bdd100053eefb8d761be0c4767ad
azerose <woojjang8671@naver.com>
2022-11-02 22:16:03 +0900

    Add: 아폴로 셋팅및 글로벌 스타일 설정
    아폴로 셋팅및 글로벌 스타일 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
+import { Global } from "@emotion/react";
 import { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+import ApolloSetting from "../src/commons/apollo";
+import { globalStyles } from "../src/commons/styles/globalStyles";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <ApolloSetting>
+        <>
+          <Global styles={globalStyles} />
+          <Component {...pageProps} />;
+        </>
+      </ApolloSetting>
+    </RecoilRoot>
+  );
 }
 
 export default MyApp;
