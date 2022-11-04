596edd5cac6e78077b025401d55cbbd2ebade21f
민겸 <4ggie1997@gmail.com>
2022-11-04 12:41:40 +0900

    ADD: 카카오맵 공통 컴포넌트 추가 및 Geolocation API 실험 (#18)
    * INIT: 초기 설정 libs 설치, .eslintignore 생성, tsconfig.json 생성
    
    프로젝트에 필요한 라이브러리 설치와 파일 생성 후 내용 기입
    
    closed#8
    
    * ADD: 카카오맵 공통 컴포넌트 추가 및 Geolocation API 실험 #16
    
    카카오맵 공통 컴포넌트 추가 및 위치 API 테스트
    
    closes #16

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,16 @@
+import { useEffect } from "react";
+import KakaoMap from "../src/components/common/kakaoMap";
+
 export default function Home() {
-  return <div>랜딩 페이지</div>;
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(`lat: ${position.coords.latitude}`);
+      console.log(`long: ${position.coords.longitude}`);
+    });
+  });
+  return (
+    <div>
+      <KakaoMap />
+    </div>
+  );
 }
