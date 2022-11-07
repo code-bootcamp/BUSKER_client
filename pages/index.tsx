034a0434b2c48b1d363e92886f352984369f840d
aggie97 <4ggie1997@gmail.com>
2022-11-07 19:06:24 +0900

    ADD: 랜딩 페이지 제작
    랜딩 페이지 제작
    
    close #33

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import styled from "@emotion/styled";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import { userPositionState } from "../src/commons/store";
+import { breakPoints } from "../src/commons/styles/globalStyles";
 
 export default function Home() {
   const router = useRouter();
@@ -17,11 +19,52 @@ export default function Home() {
     });
   }, []);
   return (
-    <>
-      <div>랜딩 페이지 입니다.</div>
+    <Wrapper>
+      <VideoBox>
+        <Video src="/streetDance.mp4" autoPlay muted loop></Video>
+      </VideoBox>
       <button onClick={async () => await router.push("/map")}>
         근처의 버스킹 확인하기
       </button>
-    </>
+    </Wrapper>
   );
 }
+
+export const Wrapper = styled.div`
+  position: relative;
+  padding-top: 100vh;
+  @media ${breakPoints.mobile} {
+    padding-top: 50vh;
+  }
+`;
+
+export const VideoBox = styled.div`
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  overflow: hidden;
+  margin: 0;
+  @media ${breakPoints.mobile} {
+    height: 50vh;
+    overflow-x: visible;
+    overflow-y: hidden;
+  }
+`;
+
+export const Video = styled.video`
+  display: block;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  min-width: 100%;
+  min-height: 100%;
+  width: auto;
+  height: auto;
+  transform: translate(-50%, -50%);
+  filter: brightness(50%);
+  @media ${breakPoints.mobile} {
+    transform: translate(-60%, -50%);
+  }
+`;
--- pages/map/index.tsx
+++ pages/map/index.tsx
@@ -1,9 +1,20 @@
+import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import { userPositionState } from "../../src/commons/store";
 import KakaoMap from "../../src/components/common/kakaoMap";
 
 export default function KaKaoMapPage() {
-  const [userPosition] = useRecoilState(userPositionState);
+  const [userPosition, setUserPosition] = useRecoilState(userPositionState);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(`lat: ${position.coords.latitude}`);
+      console.log(`long: ${position.coords.longitude}`);
+      setUserPosition({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+    });
+  }, []);
 
   return <KakaoMap position={userPosition} />;
 }
