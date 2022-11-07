461be55e0982d630fc9f56614d9e23b8b2ca6983
aggie97 <4ggie1997@gmail.com>
2022-11-07 22:20:32 +0900

    ADD: 카카오맵 타입, AOS 설치
    @types/kakaomaps, AOS 라이브러리 설치
    
    카카오맵 공통 컴포넌트 src/commons/kakaoMap 삭제
    카카오맵 공통 컴포넌트 타입 고정 ( nullabale => number )
    카카오맵 공통 컴포넌트 너비, 높이 100%
    
    선언 후 쓰이지 않는 변수 marker에 console.log() 적용으로 밑줄 삭제
    
    close #37

--- pages/map/index.tsx
+++ pages/map/index.tsx
@@ -1,3 +1,4 @@
+import styled from "@emotion/styled";
 import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import { userPositionState } from "../../src/commons/store";
@@ -16,5 +17,15 @@ export default function KaKaoMapPage() {
     });
   }, []);
 
-  return <KakaoMap position={userPosition} />;
+  return (
+    <Wrapper>
+      <KakaoMap position={userPosition} />
+    </Wrapper>
+  );
 }
+
+export const Wrapper = styled.div`
+  padding-top: 100px;
+  width: 100%;
+  height: 100vh;
+`;
