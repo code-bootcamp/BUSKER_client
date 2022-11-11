579b7b16df2177bad2a9ec78081a7dd23abfbc47
azerose <woojjang8671@naver.com>
2022-11-12 00:55:03 +0900

    FIX: 아티스트 등록 회원가입 로그인 공연등록 페이지 기능수정
    FIX: 아티스트 등록 받지 않아야할 값 address삭제 및 로컬에 담기
    DELETE: 아티스트 등록 타입의 Address제거
    FEATURE: 아티스트 등록 기능구현

--- src/components/units/artistsignup/artistsignup.container.tsx
+++ src/components/units/artistsignup/artistsignup.container.tsx
@@ -47,7 +47,6 @@ const ArtistSignupPageWrite = ({ isEdit }: IArtistSignupPageWrite) => {
   const onCompleteAddressSearch = (data: Address) => {
     setIsOpen((prev) => !prev);
     setAddress(data.address);
-    setValue("address", data.address);
     localStorage.setItem("address", JSON.stringify(data.address));
   };
 
@@ -78,7 +77,6 @@ const ArtistSignupPageWrite = ({ isEdit }: IArtistSignupPageWrite) => {
   };
 
   const onClickSignup = async (data: IFormData) => {
-    console.log(data);
     await createArtist({
       variables: {
         createArtistInput: data,
