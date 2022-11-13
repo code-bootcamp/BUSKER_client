463f8f0f127069947d476c9330d8f303190c19f1
aggie97 <4ggie1997@gmail.com>
2022-11-13 21:48:07 +0900

    FEATURE: 공연 댓글(후기)
    공연 댓글(후기) 퍼블리싱 & CRUD 기능 구현
    API가 연결되지 않는 문제로 api 연결 후 테스트는 못함
    
    closed #70

--- src/components/units/comment/item/Comment.presenter.tsx
+++ src/components/units/comment/item/Comment.presenter.tsx
@@ -1,7 +1,30 @@
+import ImageBox from "../../../common/imageBox";
 import * as S from "./Comment.styles";
+import { ICommentProps } from "./Comment.types";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 
-const CommentUI = () => {
-  return <S.Wrapper>댓글</S.Wrapper>;
+const CommentUI = (props: ICommentProps) => {
+  return (
+    <S.Wrapper>
+      <S.UserBox>
+        <ImageBox width="42px" height="42px" src={props.data.image} />
+        <S.UserName>{props.data.nickname ?? "익명"}</S.UserName>
+      </S.UserBox>
+      <S.ContentBox
+        value={props.comment}
+        onChange={props.onEditComment}
+        disabled={props.isEdit}
+      >
+        버스킹 너무 재밌어용
+      </S.ContentBox>
+      <S.EditButton onClick={props.onClickEdit(props.data.id)}>
+        <EditOutlined />
+      </S.EditButton>
+      <S.DeleteButton onClick={props.onClickDelete(props.data.id)}>
+        <DeleteOutlined />
+      </S.DeleteButton>
+    </S.Wrapper>
+  );
 };
 
 export default CommentUI;
